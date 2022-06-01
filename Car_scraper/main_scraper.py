337418e0a9c5619db1ea36c77e820a0194a3cfac
Sasa Trivic <sasatrivic92@gmail.com>
2022-06-01 21:14:38 +0200

    Changed car scraper such that it doesn't crash after an error

--- Car_scraper/main_scraper.py
+++ Car_scraper/main_scraper.py
@@ -12,21 +12,25 @@ def send_ntfy(msg):
     url = "https://ntfy.sh/VSNyDS35BgEi"
     requests.post(url=url, data=msg, headers=headers)
 
-try:
-	while True:
-		new_cars = get_new_cars.main()
-		print(f"Found {new_cars} new cars.")
-		if new_cars > 25:
-			allocated_time = randint(200,300)
-		elif new_cars > 15:
-			allocated_time = randint(400,500)
-		elif new_cars > 5:
-			allocated_time = randint(600, 800)
-		else:
-			allocated_time = randint(1000, 1200)
-		print(f"Will scrape individual cars for {allocated_time} seconds.")
-		time_left = car_scraper.main(allocated_time)
-		print(f"Finished scraping cars. Time left {time_left}. Waiting to fetch new cars.")
-		sleep(time_left + randint(20,30))
-except Exception as e:
-	send_ntfy(str(e))
+
+while True:
+    try:
+        new_cars = get_new_cars.main()
+        print(f"Found {new_cars} new cars.")
+        if new_cars > 25:
+            allocated_time = randint(200,300)
+        elif new_cars > 15:
+            allocated_time = randint(400,500)
+        elif new_cars > 5:
+            allocated_time = randint(600, 800)
+        else:
+            allocated_time = randint(1000, 1200)
+        print(f"Will scrape individual cars for {allocated_time} seconds.")
+        time_left = car_scraper.main(allocated_time)
+        print(f"Finished scraping cars. Time left {time_left}. Waiting to fetch new cars.")
+        sleep(time_left)
+    except Exception as e:
+        send_ntfy(str(e))
+    finally:
+        sleep(randint(20,30))
+
