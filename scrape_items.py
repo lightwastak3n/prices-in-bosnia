d99ab92e1b79caae436724e266936d669bbb89b3
Sasa Trivic <sasatrivic92@gmail.com>
2023-04-20 00:33:27 +0200

    Single scraper to run both tropic and konzum scrapers. Live test tomorrow.

--- scrape_items.py
+++ scrape_items.py
@@ -1,13 +1,37 @@
 from notifier.notifier import send_ntfy
 
 from item_scrapers.tropicScraper import TropicScraper
+from item_scrapers.konzumScraper import KonzumScraper
 from db_server.sql_server import Server
 
 
 server = Server()
-scraper = TropicScraper()
+tropic_scraper = TropicScraper()
+konzum_scraper = KonzumScraper()
 
-scraper.get_html()
-scraper.scrape_items()
-new_items, items_inserted = scraper.add_items_to_database(server, "tropic")
-send_ntfy(msg=f"Found {new_items} new items. {items_inserted} items scraped.", title="Tropic scraper", tags=["shopping_cart"])
+total_new = 0
+total_inserted = 0
+
+try:
+    tropic_scraper.get_html()
+    tropic_scraper.scrape_items()
+    new_items, items_inserted = tropic_scraper.add_items_to_database(server, "tropic")
+    total_new += new_items
+    total_inserted += items_inserted
+except Exception as e:
+    print(e)
+    print("Tropic scraper failed")
+
+
+try:
+    konzum_scraper.get_html()
+    konzum_scraper.scrape_items()
+    new_items, items_inserted = konzum_scraper.add_items_to_database(server, "konzum")
+    total_new += new_items
+    total_inserted += items_inserted
+except Exception as e:
+    print(e)
+    print("Konzum scraper failed")
+
+
+send_ntfy(msg=f"Found {total_new} new items. {total_inserted} items scraped.", title="Items scraper", tags=["shopping_cart"])
